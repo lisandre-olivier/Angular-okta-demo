7d194b5b33a1239403821746aa2d83dc6ca03f4e
Unknown <unknown@example.com>
2020-08-07 10:37:59 +0200

    Init commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,11 @@
+export default {
+  oidc: {
+    clientId: '0oaoi573ptBXcWJs74x6',
+    issuer: 'https://dev-657961.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:8080/implicit/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+  resourceServer: {
+    messagesUrl: 'http://localhost:8000/api/messages',
+  },
+};
