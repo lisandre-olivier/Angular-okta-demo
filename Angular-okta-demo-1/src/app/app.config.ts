99db4f49c69d8757a4f8ddb72447db2473085c75
lisandre-olivier <45368498+lisandre-olivier@users.noreply.github.com>
2020-08-28 17:01:16 +0200

    Update app.config.ts

--- Angular-okta-demo-1/src/app/app.config.ts
+++ Angular-okta-demo-1/src/app/app.config.ts
@@ -1,7 +1,7 @@
 export default {
   oidc: {
-    clientId: '0oaoi573ptBXcWJs74x6',
-    issuer: 'https://dev-657961.okta.com/oauth2/default',
+    //TODO clientId: '',
+    //TODO issuer: '',
     redirectUri: 'http://localhost:8080/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
